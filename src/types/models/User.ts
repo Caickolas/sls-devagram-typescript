bf9f4b05c85c5b143628e90dd348a4bb2186c8a3
Caick da Silva Galvão <88976384+Caickolas@users.noreply.github.com>
2022-10-24 19:17:47 -0300

    ss

---
+++ src/types/models/User.ts
@@ -0,0 +1,9 @@
+export type User = {
+    name: string,
+    email: string,
+    cognitoId: string,
+    avatar?: string,
+    followers: number,
+    following: Array<string>,
+    posts: number
+}
