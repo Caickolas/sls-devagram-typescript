c77fae36ff40e70275db0d32ee0d90b2707411e0
Caick da Silva Galvão <88976384+Caickolas@users.noreply.github.com>
2022-11-11 18:42:26 -0300

    lambda para obter dados do post

--- src/functions/post.ts
+++ src/functions/post.ts
@@ -110,3 +110,90 @@ export const toggleLike: Handler = async (event: any)
         return formatDefaultResponse(500, 'Erro ao curtir/descurtir publicacao! tente novamente ou contacte o administrador do sistema');
     }
 }
+export const postComent: Handler = async (event: any)
+    : Promise<DefaultJsonResponse> => {
+    try {
+        const { error } = validateEnvs(['POST_TABLE'])
+
+        if (error) {
+            return formatDefaultResponse(500, error)
+        }
+
+        const userId = getUserIdFromEvent(event);
+        if (!userId) {
+            return formatDefaultResponse(400, 'Usuario não encontrado')
+        }
+
+        const user = await UserModel.get({ 'cognitoId': userId });
+        if (!user) {
+            return formatDefaultResponse(400, 'Usuario não encontrado')
+        }
+
+        const { postId } = event.pathParameters;
+        const post = await PostModel.get({ id: postId });
+        if (!post) {
+            return formatDefaultResponse(400, 'Publicacao nao encontrada')
+        }
+
+        const request = JSON.parse(event.body);
+        const { coment } = request;
+
+        if (!coment || coment.length < 2) {
+            return formatDefaultResponse(400, 'Comentario invalido')
+        }
+
+        const comentObj = {
+            userId,
+            Username: user.name,
+            date: moment().format(),
+            coment
+        }
+
+        post.coments.push(comentObj);
+        await PostModel.update(post);
+        return formatDefaultResponse(200, 'Comentario adicionado com sucesso')
+
+    } catch (error) {
+        console.log('Error on post coment:', error)
+        return formatDefaultResponse(500, 'Erro ao comentar na publicacao! tente novamente ou contacte o administrador do sistema');
+    }
+}
+export const get: Handler = async (event: any)
+    : Promise<DefaultJsonResponse> => {
+    try {
+        const { POST_BUCKET, error } = validateEnvs(['POST_BUCKET',
+            'POST_TABLE'])
+
+        if (error) {
+            return formatDefaultResponse(500, error)
+        }
+
+        const userId = getUserIdFromEvent(event);
+        if (!userId) {
+            return formatDefaultResponse(400, 'Usuario não encontrado')
+        }
+
+        const user = await UserModel.get({ 'cognitoId': userId });
+        if (!user) {
+            return formatDefaultResponse(400, 'Usuario não encontrado')
+        }
+
+        const { postId } = event.pathParameters;
+        if (!postId) {
+            return formatDefaultResponse(400, 'Publicacao nao encontrada')
+        }
+
+        const post = await PostModel.get({ id: postId });
+        if (!post) {
+            return formatDefaultResponse(400, 'Publicacao nao encontrada')
+        }
+
+        post.image = await new S3Service().getImageURL(POST_BUCKET, post.image);
+        return formatDefaultResponse(200, undefined, post);
+    } catch (error) {
+        console.log('Error on get post by id:', error)
+        return formatDefaultResponse(500, 'Erro ao buscar dados da publicacao! tente novamente ou contacte o administrador do sistema');
+    }
+
+
+}
